5acd3628c6d420ad70ff67c9adb2269a6bba68b2
Patrick Ma <fivetwentysix@gmail.com>
2020-02-05 06:20:28 +0800

    Updates

---
+++ precache-manifest.c6f5c9cf09cf1fdef478490861a0fdbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7393f29aba0ce7623fe0a5a4c53af619",
+    "url": "/investment-target-tracker/index.html"
+  },
+  {
+    "revision": "951c753dcba614094494",
+    "url": "/investment-target-tracker/static/css/main.7df7da10.chunk.css"
+  },
+  {
+    "revision": "10ceaea25185b10cf627",
+    "url": "/investment-target-tracker/static/js/2.cf9c5d25.chunk.js"
+  },
+  {
+    "revision": "a52d65c95a2d7ffcb9ff212c4d38dff4",
+    "url": "/investment-target-tracker/static/js/2.cf9c5d25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "951c753dcba614094494",
+    "url": "/investment-target-tracker/static/js/main.bc2d2c09.chunk.js"
+  },
+  {
+    "revision": "8c06958a9c5c1b5dfa05",
+    "url": "/investment-target-tracker/static/js/runtime-main.bf961cb2.js"
+  }
+]);
